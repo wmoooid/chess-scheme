26f59704824af95fbf6979ea36c6b4365966c61e
wmoooid <threeqletters@yandex.ru>
2024-04-15 14:15:58 +0400

    filter footer

--- src/blocks/chess-filter/components/filter-footer/filter-footer.tsx
+++ src/blocks/chess-filter/components/filter-footer/filter-footer.tsx
@@ -1,7 +1,30 @@
+import * as stylex from '@stylexjs/stylex';
+import { Cross2Icon } from '@radix-ui/react-icons';
+import { colors } from '@/app/globals.stylex';
+
+const styles = stylex.create({
+    wrapper: {
+        display: 'flex',
+        justifyContent: 'center',
+        color: colors.grey50,
+    },
+
+    button: {
+        display: 'flex',
+        alignItems: 'center',
+        gap: '0.5rem',
+        padding: '0.25rem',
+        cursor: 'pointer',
+    },
+});
+
 export function FilterFooter() {
     return (
-        <div>
-            <div></div>
+        <div {...stylex.props(styles.wrapper)}>
+            <button {...stylex.props(styles.button)}>
+                <Cross2Icon />
+                Reset filter
+            </button>
         </div>
     );
 }
