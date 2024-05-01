9174600e1c1b9ac7925dfaadfe4891486c4d34d3
wmoooid <threeqletters@yandex.ru>
2024-05-01 16:03:43 +0400

    middleware fix

--- src/app/store/config/store.ts
+++ src/app/store/config/store.ts
@@ -1,9 +1,19 @@
 import { configureStore, createListenerMiddleware } from '@reduxjs/toolkit';
-import filterReducer from '@/components/filter/model/filter-slice';
-import apartmentsReducer from '@/blocks/apartments-filter/model/apartments-slice';
+import filterReducer, { changeFilter } from '@/components/filter/model/filter-slice';
+import apartmentsReducer, { applyFilter } from '@/blocks/apartments-filter/model/apartments-slice';
 
 export const listenerMiddleware = createListenerMiddleware();
 export const startAppListening = listenerMiddleware.startListening.withTypes<RootState, AppDispatch>();
+
+startAppListening({
+    actionCreator: changeFilter,
+    effect: async (_, listenerApi) => {
+        listenerApi.cancelActiveListeners();
+        await listenerApi.delay(50);
+        const filterState = listenerApi.getState().filter;
+        listenerApi.dispatch(applyFilter(filterState));
+    },
+});
 
 export const store = configureStore({
     reducer: {
